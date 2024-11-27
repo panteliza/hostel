e29d2e40ae93167330b57e7e6980af31a2036c14
Eliza Pant <elizapant.github@gmail.com>
2024-11-27 14:55:18 +0545

    ,bmjhgb

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Dreamer from '../components/Dreamer';
 import Reviews from '../components/Reviews';
 import Slider from '../components/Slider';
 import Gallery from '../components/Gallery';
+import Contact from '../components/Contact';
 const Home = () => {
   return (
     <div className="w-full min-h-screen flex flex-col items-center  overflow-hidden">
@@ -18,6 +19,9 @@ const Home = () => {
       </div>
       <div className="w-full flex-shrink-0 mt-0">
         <Gallery />
+      </div>
+      <div className="w-full flex-shrink-0 mt-0">
+        <Contact />
       </div>
      
       <Reviews/>
